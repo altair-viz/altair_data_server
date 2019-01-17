0e7085c387a4e5ee4aecd4343e2e4038b4a21129
Jake VanderPlas <jakevdp@google.com>
2019-01-16 23:01:39 -0800

    TST: test URL form

--- altair_data_server/tests/test_altair_server.py
+++ altair_data_server/tests/test_altair_server.py
@@ -1,3 +1,5 @@
+import re
+
 import numpy as np
 import pandas as pd
 import pytest
@@ -23,6 +25,7 @@ def test_data_server_json(data, session_context):
     spec = data_server(data)
     assert isinstance(spec, dict)
     assert list(spec.keys()) == ['url']
+    assert re.match('^https?://localhost:[0-9]+/[a-f0-9-]*\.json$', spec['url'])
     served_data = pd.read_json(spec['url'])
     assert(data.equals(served_data))
 
@@ -31,5 +34,6 @@ def test_data_server_csv(data, session_context):
     spec = data_server(data, fmt='csv')
     assert isinstance(spec, dict)
     assert list(spec.keys()) == ['url']
+    assert re.match('^https?://localhost:[0-9]+/[a-f0-9-]*\.csv$', spec['url'])
     served_data = pd.read_csv(spec['url'])
     assert(data.equals(served_data))
